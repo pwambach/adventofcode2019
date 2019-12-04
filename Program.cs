4990708d8541ac8a6f7ed71dcda3368bc48868c5
Philipp Wambach <philippwambach@googlemail.com>
2019-12-04 14:40:25 +0100

    day 4

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace adventofcode2019
         static void Main(string[] args)
         {
             // Day1 day1 = new Day1("/Users/phil/development/adventofcode2019/inputs/input1.txt");
-            Day2 day2 = new Day2("/Users/phil/development/adventofcode2019/inputs/input2.txt");
+            // Day2 day2 = new Day2("/Users/phil/development/adventofcode2019/inputs/input2.txt");
+            Day4 day4 = new Day4();
         }
     }
 }
